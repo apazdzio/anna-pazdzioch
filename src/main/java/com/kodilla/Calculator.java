6f18047cff3474dd25cf16e9f990f153a3912afe
“apazdzio <anna.pazdzioch@aviva.pl>
2017-08-03 19:26:49 +0200

    Second Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public int addAToB(int a, int b){
+        return a + b;
+    }
+    public int substractAFromB(int a, int b){
+        return b - a;
+    }
+    public static void main(String args[]){
+        System.out.println(calculator.addAToB(10, 15));
+    }
+}
