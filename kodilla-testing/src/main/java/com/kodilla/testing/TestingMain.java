124b2c208fa7f6c4390cc71f9b1b1167b21b23b4
“apazdzio <anna.pazdzioch@aviva.pl>
2017-08-07 21:41:36 +0200

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.addAToB(10, 15);
+        int difference = calculator.substractAFromB(10, 15);
+
+        if(sum == 25){
+            System.out.println("Test1 OK");
+        }else{
+            System.out.println("Test1 Error!");
+        }
+        if(difference == 5){
+            System.out.println("Test2 OK");
+        }else{
+            System.out.println("Test2 Error!");
+        }
     }
 }
