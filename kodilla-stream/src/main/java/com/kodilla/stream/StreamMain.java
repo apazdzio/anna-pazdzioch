dbdd4a5c9dd9eaf2c445376615409b5589da76fe
“apazdzio <anna.pazdzioch@aviva.pl>
2017-08-25 21:28:20 +0200

    8Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.MathExpression;
@@ -25,5 +26,12 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Zadanie 7.1");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("anna", (a) -> a.toUpperCase());
+        poemBeautifier.beautify("Anna", (a)-> a.concat(" is nice"));
+        poemBeautifier.beautify("ROBERT", (r) -> r.replace("R", "Ł"));
+        poemBeautifier.beautify("Robert", (r) -> "+++" + r + "---");
     }
 }
