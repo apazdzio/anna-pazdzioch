9bb6b92ab2bce0a5f8907ffb955b1857c7a8078a
“apazdzio <anna.pazdzioch@aviva.pl>
2017-09-10 21:55:59 +0200

    8.4,2 Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -12,21 +12,25 @@ public class FlightSearcher {
         Map<String, Boolean> flightMap = new HashMap<>();
         flightMap.put("Wroclaw", true);
         flightMap.put("Paris", true);
-        flightMap.put("Viena", true);
+        flightMap.put("Vienna", true);
         flightMap.put("London", false);
 
         boolean isPossibleToFly = false;
+        boolean isInMap = false;
         for (Map.Entry<String, Boolean> entry :flightMap.entrySet()) {
             if (flight.getArrivalAirport().equals(entry.getKey())) {
+                System.out.println("Can you fly to " + entry.getKey() + "? " + entry.getValue());
                 isPossibleToFly = entry.getValue();
+                isInMap = true;
             }
-            throw new RouteNotFoundException("Direction not found");
         };
-        return isPossibleToFly;
-
+        if(isInMap) {
+            return isPossibleToFly;
+        }
+        throw new RouteNotFoundException("Direction not found");
     }
     public static void main(String[] args){
-        Flight flight1 = new Flight("Warsaw", "Paris");
+        Flight flight1 = new Flight("Warsaw", "Moscow");
 
         FlightSearcher flightSearcher = new FlightSearcher();
         try {
