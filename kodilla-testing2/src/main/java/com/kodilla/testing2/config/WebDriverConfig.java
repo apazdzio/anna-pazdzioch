3446bc7fb894fbf525832d8c3cb3eb700dffbc30
“apazdzio <anna.pazdzioch@aviva.pl>
2017-12-11 21:30:38 +0100

    22.3 Commit

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -0,0 +1,5 @@
+package com.kodilla.testing2.config;
+
+public class WebDriverConfig {
+
+}
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing2.google;
+
+public class GoogleTestingApp {
+
+    public static void main(String[] args){
+        System.setProperty("webdriver.gecko.driver", "C:\\Selenium-drivers\\Firefox\\geckodiver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromediver.exe");
+    }
+}
